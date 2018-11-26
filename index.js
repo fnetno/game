e2b06561c5a5ce6d9e965d2bd7e4ae3db4516e95
Morgan <morgan.eychenne-fent@ada.ac.uk>
2018-11-26 10:48:30 +0000

    Migrate code from repl.it
    Here I migrated the current state of the game from repl.it, including the html css and js files.
    Currently, the game has:
    - Two player emojis
    - Player movement on the canvas
    - Gravity
    - Changing game state (for main menu, actual game, and game over screen)
    - Collision between players that doesn't work as intended (p5.js' .bounce method on the group 'players', line 54)

---
+++ index.js
@@ -0,0 +1,145 @@
+var playerX = 250;
+var playerY = 250;
+var groundY = 400;
+var jumping1 = false;
+var jumping2 = false;
+// bool of players' jumping states
+var player1;
+var player2;
+var ground;
+var gameState = 0;
+// 0 = main menu, 1 = actual game thing, 2 = game over screen
+var players;
+
+function preload() {
+	thinkImg = loadImage("assets/think.png");
+	laughImg = loadImage("assets/cry_laugh.png");
+}
+
+
+function setup() {
+	createCanvas(600,500);
+	ground = createSprite(width/2,groundY+5,width,10);
+	imageMode(CENTER);
+
+	player1 = createSprite(playerX,playerY,50,50);
+	player1.addImage(thinkImg);
+	player1.scale = 0.067;
+	player1.velocity.y = 0;
+	player1.velocity.x = 0;
+	
+
+	player2 = createSprite(playerX+75,playerY,50,50);
+	player2.addImage(laughImg);
+	player2.scale = 0.066;
+	player2.velocity.y = 0;
+	player2.velocity.x = 0;
+
+	players = new Group();
+	players.add(player1);
+	players.add(player2);
+
+
+}
+
+
+function draw() {
+	if (gameState == 0) {
+		background(40);
+		menu();
+	}
+	else if (gameState == 1) {
+		background(80);
+		playerMovement();
+		players.bounce(players)
+		drawSprites();
+		gravity();
+	}
+	else if (gameState == 2) {
+		gameOver();
+	}
+}
+
+
+function playerMovement() {
+	if (keyIsDown(37)&&player1.position.x-25>0) {
+		if (jumping1) {
+			player1.position.x -= 12;
+		}
+		else {
+			player1.position.x -= 8;
+		}
+		
+	}
+	if (keyIsDown(39)&&player1.position.x+25<width) {
+		if (jumping1) {
+			player1.position.x += 12;
+		}
+		else {
+			player1.position.x += 8;
+		}
+	}
+
+	if (keyIsDown(65)&&player2.position.x-25>0) {
+		if (jumping2) {
+			player2.position.x -= 12;
+		}
+		else {
+			player2.position.x -= 8;
+		}
+	}
+	if (keyIsDown(68)&&player2.position.x+25<width) {
+		if (jumping2) {
+			player2.position.x += 12;
+		}
+		else {
+			player2.position.x += 8;
+		}
+	}
+}
+
+
+function keyPressed() {
+	if (keyCode === 38 && !jumping1) {
+		player1.velocity.y = -14;
+		jumping1 = true;
+	}
+
+	if (keyCode === 87 && !jumping2) {
+		player2.velocity.y = -14;
+		jumping2 = true;
+	}
+
+	if (keyCode === 32 && gameState == 0) {
+		gameState = 1
+	}
+}
+
+
+function gravity() {
+	if (!player1.overlap(ground)) {
+		player1.velocity.y += 1;
+	}
+	else {
+		player1.velocity.y = 0;
+		player1.position.y = groundY-25;
+		jumping1 = false;
+	}
+
+	if (!player2.overlap(ground)) {
+		player2.velocity.y += 1;
+	}
+	else {
+		player2.velocity.y = 0;
+		player2.position.y = groundY-25;
+		jumping2 = false;
+	}
+}
+
+function menu() {
+	console.log("Maybe I should develop this part")
+}
+
+function gameOver() {
+	console.log("I should probably also develop this too")
+}
