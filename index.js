f39da3b60aa541f53e3c263173c28ce377ec2289
Morgan <morgan.eychenne-fent@ada.ac.uk>
2018-12-13 10:48:50 +0000

    Update html and change game layout
    I updated the HTML file to include the game's name and controls.
    
    The game was previously a platformer with a side-on view but due to technical restrictions regarding physics and also time restrictions on the upcoming deadline, I had to change it to a top-down game. This will make it easier to develop and play, and hopefully it will allow me to complete the game when required.

--- index.js
+++ index.js
@@ -1,50 +1,53 @@
 var playerX = 250;
 var playerY = 250;
-var groundY = 460;
-var jumping1 = false;
-var jumping2 = false;
-// bool of players' jumping states
+
+var gameState = 1;
+// 0 = main menu, 1 = actual game thing, 2 = game over screen
+// the sprites
+var players;
+var walls;
 var player1;
 var player2;
-var ground;
-var gameState = 0;
-// 0 = main menu, 1 = actual game thing, 2 = game over screen
-var players;
-var allowMovement = true;
-var gap;
+// the way the players were last moving
+var direction1;
+var direction2;
+
+
 
 function preload() {
 	thinkImg = loadImage("assets/think.png");
 	laughImg = loadImage("assets/cry_laugh.png");
 	gunImg = loadImage("assets/gun.png");
 	bg = loadImage("assets/space_background.png")
-
 }
 
 
-function setup() {
+function setup() { 
 	createCanvas(600, 500);
-	ground = createSprite(width / 2, groundY + 5, width, 10);
 	imageMode(CENTER);
 
-	player1 = createSprite(playerX, playerY, 50, 50);
+	// creating player1
+	player1 = createSprite(playerX+75, playerY, 50, 50);
 	player1.addImage(thinkImg);
 	player1.scale = 0.067;
 	player1.velocity.y = 0;
 	player1.velocity.x = 0;
 
 
-	player2 = createSprite(playerX + 75, playerY, 50, 50);
+	// creating player2
+	player2 = createSprite(playerX, playerY, 50, 50);
 	player2.addImage(laughImg);
 	player2.scale = 0.066;
 	player2.velocity.y = 0;
 	player2.velocity.x = 0;
 
+	// creating a group and adding the players to it
 	players = new Group();
 	players.add(player1);
 	players.add(player2);
 
-
+	// creating the level
+	walls = new Group();
 }
 
 
@@ -60,7 +63,6 @@ function draw() {
 		image(bg,width/2,height/2);
 		playerMovement();		
 		drawSprites();
-		gravity();
 	}
 	//game over screen
 	else if (gameState == 2) {
@@ -69,85 +71,76 @@ function draw() {
 }
 
 function playerMovement() {
-	if (keyIsDown(37) && player1.position.x - 25 > 0 && allowMovement) {
-		if (jumping1) {
-			player1.position.x -= 12;
-		}
-		else {
-			player1.position.x -= 8;
-		}
+	// player1 move left
+	if (keyIsDown(37)&&player1.position.x>25) {
+		player1.position.x -= 8;
+		direction1 = "LEFT";
 	}
-	if (keyIsDown(39) && player1.position.x + 25 < width && allowMovement) {
-		if (jumping1) {
-			player1.position.x += 12;
-		}
-		else {
-			player1.position.x += 8;
-		}
+	// player1 move right
+	if (keyIsDown(39)&&player1.position.x<width-25) {
+		player1.position.x += 8;
+		direction1 = "RIGHT"
+	}
+	// player1 move up
+	if (keyIsDown(38)&&player1.position.y>25) {
+		player1.position.y -= 8;
+		direction1 = "UP"
+	}
+	// player1 move down
+	if (keyIsDown(40)&&player1.position.y<height-25) {
+		player1.position.y += 8;
+		direction1 = "DOWN"
 	}
 
-	if (keyIsDown(65) && player2.position.x - 25 > 0 && allowMovement) {
-		if (jumping2) {
-			player2.position.x -= 12;
-		}
-		else {
-			player2.position.x -= 8;
-		}
+	// player2 move left
+	if (keyIsDown(65)&&player2.position.x>25) {
+		player2.position.x -= 8;
+		direction2 = "LEFT";
 	}
-	if (keyIsDown(68) && player2.position.x + 25 < width && allowMovement) {
-		if (jumping2) {
-			player2.position.x += 12;
-		}
-		else {
-			player2.position.x += 8;
-		}
+	// player2 move right
+	if (keyIsDown(68)&&player2.position.x<width-25) {
+		player2.position.x += 8;
+		direction2 = "RIGHT";
+	}
+	// player2 move up
+	if (keyIsDown(87)&&player2.position.y>25) {
+		player2.position.y -= 8;
+		direction2 = "UP";
+	}
+	// player2 move down
+	if (keyIsDown(83)&&player2.position.y<height-25) {
+		player2.position.y += 8;
+		direction2 = "DOWN";
 	}
 }
 
 
 function keyPressed() {
-	if (keyCode === 38 && !jumping1) {
-		player1.velocity.y = -14;
-		jumping1 = true;
-	}
-
-	if (keyCode === 87 && !jumping2) {
-		player2.velocity.y = -14;
-		jumping2 = true;
-	}
-
+	// pressing space on main menu
 	if (keyCode === 32 && gameState == 0) {
+		// enters main game
 		gameState = 1;
 	}
-}
-
-
-function gravity() {
-	if (!player1.overlap(ground)) {
-		player1.velocity.y += 1;
-	}
-	else {
-		player1.velocity.y = 0;
-		player1.position.y = groundY - 25;
-		jumping1 = false;
-	}
-
-	if (!player2.overlap(ground)) {
-		player2.velocity.y += 1;
-	}
-	else {
-		player2.velocity.y = 0;
-		player2.position.y = groundY - 25;
-		jumping2 = false;
+	// pressing space on game over screen
+	if (keyCode === 32 && gameState == 2) {
+		// enters main menu
+		gameState = 0;
 	}
 }
+
 
 function menu() {
-	
-	//Maybe I should develop this part
+	// white text displaying the name of the game and how to start
+	fill(255);
+	textSize(58);
+	textAlign(CENTER);
+	text("Emoji Game",width/2,(height/2)-50)
+
+	textSize(36);
+	text("Press SPACE to start",width/2,height/2)
 }
 
 function gameOver() {
-	//I should probably also develop this too
+	//text showing winner, time elapsed and total lives lost
 }
 
