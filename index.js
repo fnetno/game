89b76a94ccaf0fa6258b0be9ed063df311f0115a
Morgan <morgan.eychenne-fent@ada.ac.uk>
2018-12-13 08:52:58 +0000

    Loading assets
    In this change, I added some more assets (a weapon and background) to the game. I also experimented with collision between players, which failed horribly.
    p5.play's methods of collision(), bounce() and displace() all add incorrectly functioning collision as one player will be able to move the other but not vice versa. This would make the game unfair and biased towards the player who can move the other one, so I decided to not include collision between players.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var playerX = 250;
 var playerY = 250;
-var groundY = 400;
+var groundY = 460;
 var jumping1 = false;
 var jumping2 = false;
 // bool of players' jumping states
@@ -10,26 +10,31 @@ var ground;
 var gameState = 0;
 // 0 = main menu, 1 = actual game thing, 2 = game over screen
 var players;
+var allowMovement = true;
+var gap;
 
 function preload() {
 	thinkImg = loadImage("assets/think.png");
 	laughImg = loadImage("assets/cry_laugh.png");
+	gunImg = loadImage("assets/gun.png");
+	bg = loadImage("assets/space_background.png")
+
 }
 
 
 function setup() {
-	createCanvas(600,500);
-	ground = createSprite(width/2,groundY+5,width,10);
+	createCanvas(600, 500);
+	ground = createSprite(width / 2, groundY + 5, width, 10);
 	imageMode(CENTER);
 
-	player1 = createSprite(playerX,playerY,50,50);
+	player1 = createSprite(playerX, playerY, 50, 50);
 	player1.addImage(thinkImg);
 	player1.scale = 0.067;
 	player1.velocity.y = 0;
 	player1.velocity.x = 0;
-	
 
-	player2 = createSprite(playerX+75,playerY,50,50);
+
+	player2 = createSprite(playerX + 75, playerY, 50, 50);
 	player2.addImage(laughImg);
 	player2.scale = 0.066;
 	player2.velocity.y = 0;
@@ -44,34 +49,35 @@ function setup() {
 
 
 function draw() {
+	//main menu
 	if (gameState == 0) {
 		background(40);
 		menu();
 	}
+	//level with players
 	else if (gameState == 1) {
-		background(80);
-		playerMovement();
-		players.bounce(players)
+		// background(80);
+		image(bg,width/2,height/2);
+		playerMovement();		
 		drawSprites();
 		gravity();
 	}
+	//game over screen
 	else if (gameState == 2) {
 		gameOver();
 	}
 }
 
-
 function playerMovement() {
-	if (keyIsDown(37)&&player1.position.x-25>0) {
+	if (keyIsDown(37) && player1.position.x - 25 > 0 && allowMovement) {
 		if (jumping1) {
 			player1.position.x -= 12;
 		}
 		else {
 			player1.position.x -= 8;
 		}
-		
 	}
-	if (keyIsDown(39)&&player1.position.x+25<width) {
+	if (keyIsDown(39) && player1.position.x + 25 < width && allowMovement) {
 		if (jumping1) {
 			player1.position.x += 12;
 		}
@@ -80,7 +86,7 @@ function playerMovement() {
 		}
 	}
 
-	if (keyIsDown(65)&&player2.position.x-25>0) {
+	if (keyIsDown(65) && player2.position.x - 25 > 0 && allowMovement) {
 		if (jumping2) {
 			player2.position.x -= 12;
 		}
@@ -88,7 +94,7 @@ function playerMovement() {
 			player2.position.x -= 8;
 		}
 	}
-	if (keyIsDown(68)&&player2.position.x+25<width) {
+	if (keyIsDown(68) && player2.position.x + 25 < width && allowMovement) {
 		if (jumping2) {
 			player2.position.x += 12;
 		}
@@ -111,7 +117,7 @@ function keyPressed() {
 	}
 
 	if (keyCode === 32 && gameState == 0) {
-		gameState = 1
+		gameState = 1;
 	}
 }
 
@@ -122,7 +128,7 @@ function gravity() {
 	}
 	else {
 		player1.velocity.y = 0;
-		player1.position.y = groundY-25;
+		player1.position.y = groundY - 25;
 		jumping1 = false;
 	}
 
@@ -131,15 +137,17 @@ function gravity() {
 	}
 	else {
 		player2.velocity.y = 0;
-		player2.position.y = groundY-25;
+		player2.position.y = groundY - 25;
 		jumping2 = false;
 	}
 }
 
 function menu() {
-	console.log("Maybe I should develop this part")
+	
+	//Maybe I should develop this part
 }
 
 function gameOver() {
-	console.log("I should probably also develop this too")
+	//I should probably also develop this too
 }
+
